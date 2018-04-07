fbe9465f23cb5221d2da5ec2cd61a9ad68a6bd4c
Konstantin Bezgodov <kons.bezg@gmail.com>
2018-04-07 21:25:56 +0500

    [wip]

---
+++ Assets/Scripts/BitcoinController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitcoinController : MonoBehaviour { 
+	public GameObject UI;
+	private GameManager gm;
+	// Use this for initialization
+	void Start () {
+		gm = Camera.main.GetComponent<GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		gm.IncrementScore ();
+		Destroy (gameObject);
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+	private int score;
+	public Text text;
+
+	public void IncrementScore() {
+		score += 10;
+		text.text = "score: " + score;
+	}
+
     public void ChangeMenuToGame()
     {
         SceneManager.LoadScene("Main");
