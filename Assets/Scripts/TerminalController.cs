e77da11b39575a750a2e413bbf898c02759bb6a6
Konstantin Bezgodov <kons.bezg@gmail.com>
2018-04-07 16:12:34 +0500

    added start end end terminal

---
+++ Assets/Scripts/TerminalController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalController : MonoBehaviour {
+	//public GameManager gm;
+	public bool isEnd;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public Vector3 getStartPosition() {
+		return transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (isEnd) {
+			//gm.levelSuccess ();
+		}
+	}
+}
